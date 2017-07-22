d20a200c2eaefd592963bfc46060798f4bdc9f0e
OGNeekoPolo <nktrabue@gmail.com>
2017-07-22 18:14:25 -0400

    Even more tweaks

--- starter_files/lib/blackjack.rb
+++ starter_files/lib/blackjack.rb
@@ -1,8 +1,8 @@
 require_relative 'deck'
-# require "pry"
+
 class Game
 
-  attr_accessor :player_hand, :dealer_hand, :deck, :player_value, :dealer_value, :win_tracker, :loss_tracker, :bank
+  attr_accessor :player_hand, :dealer_hand, :deck, :player_value, :dealer_value, :win_tracker, :loss_tracker, :bank, :bet
 
   def initialize
     @win_tracker = 0
@@ -15,16 +15,18 @@ class Game
   def play_game
     puts "Ready to play? [Press Enter to continue]"
     gets
-    @bank -= 10
-    puts "$10 Bet Placed"
-    puts "Remaining Money: #{@bank}"
+    puts "Your bank is at $#{@bank}"
+    puts "Enter your bet"
+    @bet = gets.chomp.to_i
+    @bank -= @bet
+    puts "$#{bet} Bet Placed"
+    puts "Remaining Money: $#{@bank}"
     set_deck
     deal_cards
     player_phase
     dealer_phase
     new_game?
   end
-
 
   def set_deck
     @player_hand = []
@@ -43,23 +45,19 @@ class Game
     end
   end
 
-
-  def total_dealer_value
-  end
-
   def player_phase
     @player_value = @player_hand.reduce(0) {|sum, value| sum + value.value}
     @dealer_value = @dealer_hand.reduce(0) {|sum, value| sum + value.value}
     if @player_value == 21
       puts "Player has BlackJack!! You Win!!!"
       @win_tracker += 1
-      @bank += 20
+      @bank += (@bet * 2)
       new_game?
     else
       puts "Your hand has a value of #{@player_value}."
       puts "It is made up of a #{@player_hand[0]} and a #{@player_hand[1]}."
       puts "The dealer is showing that their first card is a #{@dealer_hand[0]}."
-      puts "Would you like a hit? (y / n)"
+      puts "Would you like a hit? Press (y / n)"
       hit = gets.chomp.downcase
       if hit == "y"
         hit(@player_hand)
@@ -78,19 +76,18 @@ class Game
       puts "The dealer is about to hit....[Press Enter to proceed]"
       gets
       hit(dealer_hand)
-      @dealer_value = @dealer_hand.reduce(0) {|sum, value| sum + value.value}
       if @dealer_value < 17
         hit(dealer_hand)
       elsif @dealer_value > 21
         puts "Dealer busted with #{@dealer_value}. YOU WIN!!!"
         @win_tracker += 1
-        @bank += 20
+        @bank += (@bet * 2)
         new_game?
-      elsif @dealer_value > 17 && @dealer_value < @player_value
+      elsif @dealer_value >= 17 && @dealer_value < @player_value
         puts "Your hand of #{@player_value} beats the dealers hand of #{@dealer_value}"
         puts "YOU WIN!!!"
         @win_tracker += 1
-        @bank += 20
+        @bank += (@bet * 2)
         new_game?
       elsif @dealer_value > @player_value
         puts "The dealer has a hand of #{@dealer_value} which beats your hand of #{@player_value}"
@@ -101,24 +98,24 @@ class Game
         puts "You and the dealer have the same value of #{@player_value}"
         puts "That was close....but YOU WIN!!!"
         @win_tracker += 1
-        @bank += 20
+        @bank += (@bet * 2)
         new_game?
       end
     elsif @dealer_value == 21 && @dealer_value > @player_value
       puts "Dealer has blackjack. YOU LOSE!!"
       @loss_tracker += 1
       new_game?
-    elsif @dealer_value > 17 && @dealer_value < @player_value
+    elsif @dealer_value >= 17 && @dealer_value < @player_value
       puts "Your hand of #{@player_value} is better than the dealers hand of #{@dealer_value}"
       puts "You WIN!!!"
       @win_tracker += 1
-      @bank += 20
+      @bank += (@bet * 2)
       new_game?
-    elsif @dealer_value > 17 && @dealer_value == @player_value
+    elsif @dealer_value >= 17 && @dealer_value == @player_value
       puts "You and the dealer have the same value of #{@player_value}"
       puts "That was close....but YOU WIN!!!"
       @win_tracker += 1
-      @bank += 20
+      @bank += (@bet * 2)
       new_game?
     elsif @dealer_value > 17 && @dealer_value > @player_value
       puts "The dealers hand of #{@dealer_value} is better than your hand of #{@player_value}"
@@ -133,7 +130,8 @@ class Game
       @player_hand << @deck.draw_card
       @player_value = @player_hand.reduce(0) {|sum, value| sum + value.value}
       if @player_value <= 21
-        puts "You ended up drawing a #{@player_hand[2]}"
+        num = @player_hand.length
+        puts "You ended up drawing a #{@player_hand[num - 1]}"
         puts "Your hand has a new value of #{@player_value}."
         puts "Would you like another hit?"
         hit = gets.chomp.downcase
@@ -148,7 +146,11 @@ class Game
         new_game?
       end
     else
+      dcard = @dealer_hand.length
       @dealer_hand << @deck.draw_card
+      @dealer_value = @dealer_hand.reduce(0) {|sum, value| sum + value.value}
+      puts "The dealer drew a #{@dealer_hand[dcard - 1]}"
+      puts "The dealers new value is #{@dealer_value}"
     end
   end
 
@@ -173,5 +175,4 @@ class Game
   end
 end
 
-# binding.pry
 Game.new.play_game
